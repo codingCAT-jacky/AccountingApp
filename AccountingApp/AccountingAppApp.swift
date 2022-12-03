1120e6d5cc99d69a5954902184bc0b41026d89b5
codingCAT-jacky <76811156+codingCAT-jacky@users.noreply.github.com>
2022-12-04 01:51:29 +0800

    Initial Commit

---
+++ AccountingApp/AccountingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountingAppApp.swift
+//  AccountingApp
+//
+//  Created by pads on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct AccountingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
